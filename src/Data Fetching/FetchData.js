bbe6f8670ba21df92ac1cb441c13265c181b5e54
Azamat <jumamuratovazamat2004@gmail.com>
2026-03-28 15:11:13 +0500

    изменил base_url

--- src/Data Fetching/FetchData.js
+++ src/Data Fetching/FetchData.js
@@ -1,4 +1,4 @@
-const base_url = "https://azamatjumamuratov.pythonanywhere.com/api/";
+const base_url = "https://aza.imaster.uz/api/";
 
 export default async function FetchData(path, options) {
   return fetch(base_url + path, options);
